7a384e9bf06609105a4db9c0253da9b2ae12cfb4
aymane <oujdimraymane@gmail.com>
2024-03-19 23:06:01 +0000

    update helpers

--- server/app/helpers.php
+++ server/app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
+
 if (!function_exists('apiResponse')) {
     function apiResponse($data = [], $statusCode = 200, $headers = [])
     {
@@ -13,5 +15,20 @@ if (!function_exists('saveImage')) {
         $pathName = time() . '_' . $image->getClientOriginalName();
         $image->storeAs('public/products_images', $pathName);
         return $pathName;
+    }
+}
+
+if (!function_exists('updateImage')) {
+    function updateImage($oldImagePath, $newImage)
+    {
+        if ($newImage !== null) {
+            if (Storage::exists('public/products_images/' . $oldImagePath)) {
+                Storage::delete('public/products_images/' . $oldImagePath);
+            }
+            $pathName = time() . '_' . $newImage->getClientOriginalName();
+            $newImage->storeAs('public/products_images', $pathName);
+            return $pathName;
+        }
+        return $oldImagePath;
     }
 }
